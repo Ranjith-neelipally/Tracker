06cf52c991208cb0580b6b80de937cc983ef4789
Ranjith <ranjith.neelipally@ibigroup.com>
2024-08-18 22:49:02 +0530

    reverted babel config

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,4 @@
 module.exports = {
-  presets: [
-    'module:@react-native/babel-preset',
-    'module:metro-react-native-babel-preset',
-  ],
-  plugins: [
-    'react-native-reanimated/plugin',
-    ['@babel/plugin-proposal-decorators', {legacy: true}],
-  ],
+  presets: ['module:@react-native/babel-preset'],
+  plugins: ['react-native-reanimated/plugin'],
 };
